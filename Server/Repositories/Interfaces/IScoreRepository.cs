f09194c4ea3a293b251b4fb7ddb5fc6280d77c20
its-dax <ddax.kn@gmail.com>
2022-05-03 11:55:20 +0200

    Add score repository

---
+++ Server/Repositories/Interfaces/IScoreRepository.cs
@@ -0,0 +1,15 @@
+﻿using GamificationApp.Shared.Models;
+
+namespace GamificationApp.Server.Repositories.Interfaces
+{
+    public interface IScoreRepository
+    {
+
+        Task<IEnumerable<Score>> GetScores();
+        Task<IEnumerable<Score>> GetScore(int id);
+        Task<IEnumerable<Subject>> GetSubjects();
+        Task<IEnumerable<Subject>> GetSubject(int id);
+        Task<IEnumerable<User>> GetUsers();
+        Task<IEnumerable<User>> GetUser(int id);
+    }
+}
---
+++ Server/Repositories/ScoreRepository.cs
@@ -0,0 +1,50 @@
+﻿using GamificationApp.Server.Data;
+using GamificationApp.Server.Repositories.Interfaces;
+using GamificationApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamificationApp.Server.Repositories
+{
+    public class ScoreRepository : IScoreRepository
+
+    {
+        private readonly DataContext _dataContext;
+
+        public ScoreRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public Task<IEnumerable<Score>> GetScore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Score>> GetScores()
+        {
+            var scores = await _dataContext.Scores.ToListAsync();
+            return scores;
+        }
+
+        public Task<IEnumerable<Subject>> GetSubject(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Subject>> GetSubjects()
+        {
+            var subjects = await _dataContext.Subjects.ToListAsync();
+            return subjects;
+        }
+
+        public Task<IEnumerable<User>> GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+            return users;
+        }
+    }
+}
